7f6fea3296f3225413eb757c3415618cc3acfd38
Hugo Ribeiro <hugolerib@gmail.com>
2021-06-15 13:09:16 -0300

    Parte 3 - Definindo Comportamento

---
+++ Java_OO_Introducao/bytebank/src/application/TestaMetodo.java
@@ -0,0 +1,26 @@
+package application;
+
+import entities.Conta;
+
+public class TestaMetodo {
+	public static void main(String[] args) {
+		Conta contaDoHugo = new Conta();
+		contaDoHugo.saldo = 100;
+		contaDoHugo.deposita(50);
+		System.out.println(contaDoHugo.saldo);
+		boolean conseguiuRetirar = contaDoHugo.saca(70);
+		System.out.println(contaDoHugo.saldo);
+		System.out.println(conseguiuRetirar);
+		
+		Conta contaDoJoao = new Conta();
+		contaDoJoao.deposita(1000);
+		
+		if (contaDoJoao.transfere(200, contaDoHugo)) {
+			System.out.println("Transferência feita com sucesso");
+		} else {
+			System.out.println("Faltou dinheiro");
+		}
+		System.out.println(contaDoJoao.saldo);
+		System.out.println(contaDoHugo.saldo);
+	}
+}
--- Java_OO_Introducao/bytebank/src/entities/Conta.java
+++ Java_OO_Introducao/bytebank/src/entities/Conta.java
@@ -6,6 +6,29 @@ public class Conta {
 	public int numero;
 	public String titular;
 	
+	public void deposita(double valor) {
+		this.saldo += valor;
+	}
+	
+	public boolean saca(double valor) {
+		if (this.saldo >= valor) {
+			this.saldo -= valor;
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public boolean transfere(double valor, Conta contaDestino) {
+		if (this.saldo >= valor) {
+			this.saldo -= valor;
+			contaDestino.deposita(valor);
+			return true;
+		}
+		return false;	
+	}
+	
 	public double getSaldo() {
 		return saldo;
 	}
