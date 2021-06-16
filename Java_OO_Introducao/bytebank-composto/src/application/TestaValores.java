2b3135bc3523c6d5e351c690bc51ee34db870c7a
Hugo Ribeiro <hugolerib@gmail.com>
2021-06-15 23:13:37 -0300

    Parte 6 - Construtores e Static

---
+++ Java_OO_Introducao/bytebank-composto/src/application/TestaValores.java
@@ -0,0 +1,17 @@
+package application;
+
+import entities.Conta;
+
+public class TestaValores {
+	
+	public static void main(String[] args) {
+		Conta conta = new Conta(1337, 24226);
+		
+		System.out.println(conta.getAgencia());
+		
+		Conta conta2 = new Conta(1337, 16549);
+		
+		System.out.println(Conta.getTotalDeContas());
+	}
+
+}
--- Java_OO_Introducao/bytebank-composto/src/entities/Conta.java
+++ Java_OO_Introducao/bytebank-composto/src/entities/Conta.java
@@ -5,6 +5,19 @@ public class Conta {
 	private int agencia;
 	private int numero;
 	private Cliente titular;
+	private static int totalDeContas;
+	
+	public Conta() {
+		
+	}
+	
+	public Conta(int agencia, int numero) {
+		totalDeContas++;
+//		System.out.println("O total de contas é: " + Conta.totalDeContas);
+		this.agencia = agencia;
+		this.numero = numero;
+//		System.out.println("estou criando uma conta " + this.numero);
+	}
 	
 	
 	public void deposita(double valor) {
@@ -38,12 +51,20 @@ public class Conta {
 		return agencia;
 	}
 	public void setAgencia(int agencia) {
+		if (agencia <= 0) {
+			System.out.println("Não pode valor menor ou igual a zero");
+			return;
+		}
 		this.agencia = agencia;
 	}
 	public int getNumero() {
 		return numero;
 	}
 	public void setNumero(int numero) {
+		if (numero <= 0) {
+			System.out.println("Não pode valor <= a 0");
+			return;
+		}
 		this.numero = numero;
 	}
 	public Cliente getTitular() {
@@ -52,6 +73,10 @@ public class Conta {
 
 	public void setTitular(Cliente titular) {
 		this.titular = titular;
+	}
+	
+	public static int getTotalDeContas() {
+		return Conta.totalDeContas;
 	}
 	
 }
