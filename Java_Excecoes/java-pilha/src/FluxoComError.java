4fb75df330bba7d65ebf800a093bfae6a8d24f18
Hugo Ribeiro <hugolerib@gmail.com>
2021-06-23 23:45:37 -0300

    Parte 4 - Excecoes

--- Java_Excecoes/java-pilha/src/FluxoComError.java
+++ Java_Excecoes/java-pilha/src/FluxoComError.java
@@ -1,4 +1,4 @@
-public class Fluxo {
+public class FluxoComError {
 
     public static void main(String[] args) {
         System.out.println("Ini do main");
@@ -20,8 +20,9 @@ public class Fluxo {
     }
 
     private static void metodo2() {
-        System.out.println("Ini do metodo2");
-        throw new MinhaExcecao("Deu errado");
+    	// StackOverFlow
+    	System.out.println("Chamando metodo 2");
+    	metodo2();
         
 //        System.out.println("Fim do metodo2");
     }
