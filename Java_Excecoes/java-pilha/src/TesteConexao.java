d47af31ac18a8ddb9c02d06d2bf37fc50b2009a9
Hugo Ribeiro <hugolerib@gmail.com>
2021-06-24 01:44:02 -0300

    Parte 6 - Finally e try

---
+++ Java_Excecoes/java-pilha/src/TesteConexao.java
@@ -0,0 +1,25 @@
+
+public class TesteConexao {
+	
+	public static void main(String[] args) {
+		
+		try (Conexao conexao = new Conexao()){
+			conexao.leDados();		
+		} catch(IllegalStateException ex) {
+			System.out.println("Deu erro na conexao");
+		}
+		
+		// ------ o código de cima é uma simplificação do código de baixo --------
+		
+//		Conexao con = null;
+//		try {
+//			con = new Conexao();
+//			con.leDados();
+//		} catch(IllegalStateException ex) {
+//			System.out.println("Deu erro na conexao");
+//		} finally {
+//			con.close();
+//		}
+	}
+
+}
