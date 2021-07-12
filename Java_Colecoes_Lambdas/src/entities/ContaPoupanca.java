3dfcda441ba8b1be11287b12a2c6f20d723e0109
Hugo Ribeiro <hugolerib@gmail.com>
2021-07-12 13:59:51 -0300

    Ordenações de lista

--- Java_Colecoes_Lambdas/src/entities/ContaPoupanca.java
+++ Java_Colecoes_Lambdas/src/entities/ContaPoupanca.java
@@ -6,5 +6,10 @@ public class ContaPoupanca extends Conta{
 	public ContaPoupanca(int agencia, int numero) {
 		super(agencia, numero);
 	}
-	
+
+	@Override
+	public String toString(){
+		return "Conta Poupanca, Numero: " + this.getNumero() + ", Agencia: " + this.getAgencia();
+	}
+
 }
