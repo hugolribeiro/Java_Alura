6bd92bce260ad79dfc60c3424e481a57e6184082
Hugo Ribeiro <hugolerib@gmail.com>
2021-07-14 12:22:01 -0300

    Testes com excpetion

--- Java_TDD_JUnit/2108-java-testes-unitarios-tdd-projeto_inicial/src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
+++ Java_TDD_JUnit/2108-java-testes-unitarios-tdd-projeto_inicial/src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
@@ -1,20 +1,34 @@
 package br.com.alura.tdd.service;
 
 import br.com.alura.tdd.modelo.Funcionario;
-import  static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class BonusServiceTest {
 
     @Test
     void bonusDeveriaSerZeroParaFuncionarioComSalarioMuitoAlto(){
         BonusService service = new BonusService();
-        BigDecimal bonus = service.calcularBonus(new Funcionario("Hugo", LocalDate.now(), new BigDecimal("25000")));
 
-        assertEquals(new BigDecimal("0.00"), bonus);
+        //Uma maneira de checar uma exception é essa:
+        assertThrows(IllegalArgumentException.class,
+                () -> service.calcularBonus(new Funcionario(
+                        "Hugo", LocalDate.now(), new BigDecimal("25000")
+                )));
+
+        // Outra maneira é essa:
+//        try{
+//            service.calcularBonus(new Funcionario(
+//                    "Hugo", LocalDate.now(), new BigDecimal("25000")));
+//            fail("Não deu a exception!");
+//        } catch (Exception e){
+//            assertEquals("Funcionario com salario maior do que R$10000 nao pode receber bonus!", e.getMessage());
+//        }
     }
 
     @Test
