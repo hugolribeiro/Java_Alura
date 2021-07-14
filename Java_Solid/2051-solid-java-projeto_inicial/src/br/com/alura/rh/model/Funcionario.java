bb2cc01467677f6997a9497a12ab59579ff4d2a8
Hugo Ribeiro <hugolerib@gmail.com>
2021-07-13 22:37:50 -0300

    Implementação de Dados Pessoais

--- Java_Solid/2051-solid-java-projeto_inicial/src/br/com/alura/rh/model/Funcionario.java
+++ Java_Solid/2051-solid-java-projeto_inicial/src/br/com/alura/rh/model/Funcionario.java
@@ -1,62 +1,26 @@
 package br.com.alura.rh.model;
 
 import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.time.LocalDate;
 
 
-import br.com.alura.rh.exceptions.ValidacaoException;
 
 public class Funcionario {
 
-	private String nome;
-	private String cpf;
-	private Cargo cargo;
-	private BigDecimal salario;
+	private DadosPessoais dadosPessoais;
 	private LocalDate dataUltimoReajuste;
 
-	public Funcionario(String nome, String cpf, Cargo cargo, BigDecimal salario) {
-		this.nome = nome;
-		this.cpf = cpf;
-		this.cargo = cargo;
-		this.salario = salario;
+	public Funcionario(String nome, String cpf, Cargo cargo, BigDecimal salario, LocalDate dataUltimoReajuste) {
+		this.dadosPessoais = new DadosPessoais(nome, cpf, cargo, salario);
+		this.dataUltimoReajuste = dataUltimoReajuste;
 	}
 
-	public void atualizarSalario(BigDecimal novoSalario) {
-		this.salario = novoSalario;
-		this.dataUltimoReajuste = LocalDate.now();
+	public DadosPessoais getDadosPessoais() {
+		return dadosPessoais;
 	}
 
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getCpf() {
-		return cpf;
-	}
-
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
-
-	public Cargo getCargo() {
-		return cargo;
-	}
-
-	public void setCargo(Cargo cargo) {
-		this.cargo = cargo;
-	}
-
-	public BigDecimal getSalario() {
-		return salario;
-	}
-
-	public void setSalario(BigDecimal salario) {
-		this.salario = salario;
+	public void setDadosPessoais(DadosPessoais dadosPessoais) {
+		this.dadosPessoais = dadosPessoais;
 	}
 
 	public LocalDate getDataUltimoReajuste() {
@@ -68,6 +32,6 @@ public class Funcionario {
 	}
 
 	public void promover(Cargo novoCargo) {
-		this.cargo = novoCargo;
+		this.dadosPessoais.setCargo(novoCargo);
 	}
 }
