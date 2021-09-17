b7e2f014f40ca90eb65b6b32514bf7d66befbe54
Hugo Ribeiro <hugolerib@gmail.com>
2021-09-16 23:31:57 -0300

    feat: spring boot admin

--- Spring_Boot_Seguranca/forum/src/main/java/br/com/alura/forum/controller/AuthenticationController.java
+++ Spring_Boot_Seguranca/forum/src/main/java/br/com/alura/forum/controller/AuthenticationController.java
@@ -10,6 +10,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,7 +29,7 @@ public class AuthenticationController {
     private TokenService tokenService;
 
     @PostMapping
-    public ResponseEntity<TokenDTO> authenticate(@RequestBody @Valid LoginForm form) {
+    public ResponseEntity<TokenDTO> authenticate(@RequestBody @Validated LoginForm form) {
         UsernamePasswordAuthenticationToken dadosLogin = form.converter();
 
         try {
